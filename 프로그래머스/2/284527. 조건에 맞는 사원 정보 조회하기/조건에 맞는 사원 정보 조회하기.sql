9c882d6f52eb10c4fa6d7b8db35748d5e755f47a
김보현 <126312003+Kim-coding@users.noreply.github.com>
2025-06-20 16:28:30 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,32 @@
+WITH SUM_SCORE AS (
+    SELECT
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM 
+        HR_GRADE 
+    WHERE
+        YEAR = 2022
+    GROUP BY 
+        EMP_NO
+),
+MAX_SCORE AS(
+    SELECT
+        MAX(SCORE) AS MAX_SCORE
+    FROM SUM_SCORE
+)
+
+SELECT 
+    S.SCORE, 
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    E.POSITION, 
+    E.EMAIL
+FROM 
+    SUM_SCORE AS S  
+JOIN 
+    HR_EMPLOYEES AS E
+    ON S.EMP_NO = E.EMP_NO
+JOIN
+    MAX_SCORE AS M
+    ON S.SCORE = M.MAX_SCORE;
+    
